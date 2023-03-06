3235b44dfd6a2979fef4292fabf2bc7ad5812c90
SirCheruiyot <cheruiyotkian@gmail.com>
2023-03-06 20:56:25 +0300

    Manipulating Complex Objects

---
+++ index92.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+  {
+      "artist": "Black Panther",
+      "title": "Wakanda",
+      "release_year": 2023,
+      "formats": [
+        "3D",
+        "HD",
+        "4K"
+      ],
+      }
+    ];

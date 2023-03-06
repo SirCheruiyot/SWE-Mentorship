6972fe0ef267b35a8aea072852b06921a4dc4c05
SirCheruiyot <cheruiyotkian@gmail.com>
2023-03-06 20:08:14 +0300

    Build JavaScript Objects

---
+++ index83.js
@@ -0,0 +1,10 @@
+const myDog = {
+    // Only change code below this line
+  
+    "name": "Skinny",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Puppies", "Dogs"]
+  
+    // Only change code above this line
+  };

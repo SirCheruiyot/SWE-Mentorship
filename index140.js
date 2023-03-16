9407f6428baf84ee02a5ec5330b7b87522245678
SirCheruiyot <cheruiyotkian@gmail.com>
2023-03-16 07:48:29 +0300

    Handle a Fulfilled Promise with then

---
+++ index140.js
@@ -0,0 +1,13 @@
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+makeServerRequest.then(result => {
+  console.log (result)
+  }
+);
